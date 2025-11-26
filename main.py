a5b97a2503d028b0001cd3be4cdb1b0c89347139
andreas00e <andreas.ehrensberger@tum.de>
2025-11-26 15:05:01 +0100

    Changes in main.py and train config file

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
+OmegaConf.register_new_resolver('len', lambda x: len(x))
+OmegaConf.register_new_resolver('add', lambda x, y: x + y)
 
 import torch
 from torch.utils.data import DataLoader, random_split
-torch.set_float32_matmul_precision("medium")
+torch.set_float32_matmul_precision('medium')
 
 import lightning as L 
 from lightning.pytorch.loggers.wandb import WandbLogger
 from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.profilers import SimpleProfiler
 from lightning.pytorch.plugins.environments import SLURMEnvironment
-SLURMEnvironment.detect = lambda: False # suppress SLURM warning from: https://github.com/Lightning-AI/pytorch-lightning/issues/6389
+SLURMEnvironment.detect = lambda: False # suppress SLURM warning
 
 from models.models import TransformerModel
 from data.data_loading import PlayDataset
+from data.utils import collate_fn
 
-@hydra.main(config_path="./conf", config_name="train", version_base=None)
+@hydra.main(config_path='./conf', config_name='train', version_base=None)
 def main(cfg: DictConfig): 
     L.seed_everything(**cfg.seed_everything)
     
@@ -23,11 +26,13 @@ def main(cfg: DictConfig):
          
     train_dataset, val_dataset = random_split(dataset=dataset, lengths=cfg.data.dataloading.lengths)
     del cfg.data.dataloading.lengths
-    train_dataloader = DataLoader(dataset=train_dataset, **cfg.data.dataloading)
+    train_dataloader = DataLoader(dataset=train_dataset, **cfg.data.dataloading, collate_fn=collate_fn)
+    
     del cfg.data.dataloading.shuffle
-    val_dataloader = DataLoader(dataset=val_dataset, **cfg.data.dataloading) 
-        
-    model = TransformerModel(feature_config=cfg.feature_config, transformer=cfg.model.transformer, in_emb=cfg.model.i, out_emb=cfg.model.o)
+    val_dataloader = DataLoader(dataset=val_dataset, **cfg.data.dataloading, collate_fn=collate_fn)     
+
+    model = TransformerModel(feature_config=cfg.feature_config, window_size=cfg.model.window_size, 
+                             transformer=cfg.model.transformer, in_emb=cfg.model.i, out_emb=cfg.model.o)
     logger = WandbLogger(**cfg.logger)
     modelCheckpoint = ModelCheckpoint(**cfg.modelCheckpoint)
     profiler = SimpleProfiler(**cfg.profiler)
