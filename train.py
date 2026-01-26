3d2ef78653869f409a75121656cbe89bae9a4fd1
andreas00e <andreas.ehrensberger@tum.de>
2026-01-26 16:50:54 +0100

    Updated train.py

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-import os 
 import hydra
 from omegaconf import DictConfig, OmegaConf
 OmegaConf.register_new_resolver("len", lambda x: len(x))
@@ -16,33 +15,29 @@ from lightning.pytorch.profilers import SimpleProfiler
 from lightning.pytorch.plugins.environments import SLURMEnvironment
 SLURMEnvironment.detect = lambda: False # suppress SLURM warning
 
-from models.models import DecoderOnlyTransformer
+from data.utils import collate_fn_graph
 from data.polars_data_loading import PlayDataset
-from data.utils import collate_fn
+from models.models_copy import DecoderOnlyTransformer
 
 
 @hydra.main(config_path="./conf", config_name="train", version_base=None)
 def main(cfg: DictConfig): 
     L.seed_everything(**cfg.seed_everything)
     
-    dataset = PlayDataset(**cfg.data.dataset, feature_config=cfg.feature_config, data_type=cfg.data.data_type)
-         
-    train_dataset, val_dataset = random_split(dataset=dataset, lengths=cfg.data.dataloading.lengths)
-    del cfg.data.dataloading.lengths
-    train_dataloader = DataLoader(dataset=train_dataset, **cfg.data.dataloading, collate_fn=collate_fn, drop_last=True)
-    
+    dataset = PlayDataset(**cfg.data.dataset)
+    train_dataset, val_dataset = random_split(dataset=dataset, lengths=cfg.data.random_split.lengths)
+    train_dataloader = DataLoader(dataset=train_dataset, **cfg.data.dataloading, collate_fn=collate_fn_graph)
     del cfg.data.dataloading.shuffle
-    val_dataloader = DataLoader(dataset=val_dataset, **cfg.data.dataloading, collate_fn=collate_fn, drop_last=True)  
+    val_dataloader = DataLoader(dataset=val_dataset, **cfg.data.dataloading, collate_fn=collate_fn_graph) 
 
-    # model = TransformerModel(feature_config=cfg.feature_config, transformer_conf=cfg.model.transformer, in_emb=cfg.model.i, out_emb=cfg.model.o)
-    model = DecoderOnlyTransformer(model_conf=cfg.model, in_emb=cfg.model.i)
+    model = DecoderOnlyTransformer(**cfg.model)
     logger = WandbLogger(**cfg.logger)
     modelCheckpoint = ModelCheckpoint(**cfg.modelCheckpoint)
     profiler = SimpleProfiler(**cfg.profiler)
-    trainer  = L.Trainer(logger=logger, callbacks=[modelCheckpoint], profiler=profiler, **cfg.trainer)
+    trainer = L.Trainer(logger=logger, callbacks=[modelCheckpoint], profiler=profiler, **cfg.trainer)
     
     trainer.fit(model=model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
-    
+
     
 if __name__ == "__main__": 
     main() 
