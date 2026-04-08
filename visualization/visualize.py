8fcb80831ce4a796f984203a1e3d30d3aea5b04c
andreas00e <andreas.ehrensberger@tum.de>
2026-04-08 12:31:43 +0200

    Little bit of refactoring and rearranging

--- visualization/visualize.py
+++ visualization/visualize.py
@@ -37,118 +37,130 @@ class Visualize():
         self.extrema = extrema
                 
         self.animation = animation
-
-        self.n_players, self.n_out_players, self.i_frames, self.o_frames = [0]*4
-        self.out_players, self.x, self.y = [], [], []
-        self.last_node_positions, self.trace_lines = {}, {}
-        self.shapes = defaultdict(dict)
+            
+        self.i_frames, self.o_frames = [0]*2
+        self.id2info, self.play = {}, {}
         
-        self.players = self._process_play()
-        self.G = self._create_graph(self.players)
+        self._process_play()
+        self.G = self._create_graph()
         
         self.fig, self.ax = plt.subplots(figsize=(self.x_size, self.y_size))
         self.text, self.anim = [None]*2 # later text and animation
-         
-    def _process_play(self) -> Dict[str, Tuple[bool, str, str]]:
+             
+    def _process_play(self) -> None:
         self.i_play[list(self.features_norm)] = self._normalize(self.i_play[list(self.features_norm)], self.extrema)
         self.o_play[["x", "y"]] = self._normalize(self.o_play[["x", "y"]], self.extrema)
         
-        i_play = self.i_play.filter(pl.col("frame_id") == 1)
-        predictions, directions, names, positions, sides = i_play["player_to_predict", "play_direction", "player_name", "player_position", "player_side"]
-        names = [name.split()[1] for name in names]
-        sides = ["red" if side == "Offense" else "blue" for side in sides]
-        shapes = ["o" if side == "Offense" else ">" if direction == "left" else "<" for side, direction in zip(sides, directions)]
-        players = dict(zip(names, zip(predictions, shapes, positions, sides)))
+        self.i_frames = int(self.i_play["frame_id"].n_unique())
+        self.o_frames = int(self.o_play["frame_id"].n_unique())
+        
+        predictions, ids, _, names, _, sides = \
+            self.i_play.filter(pl.col("frame_id") == 1)[["player_to_predict", "nfl_id", "play_direction", "player_name", "player_position", "player_side"]]
+        
+        names = names.map_elements(lambda x: x.split()[-1], return_dtype=pl.String)
+        colors = sides.map_elements(lambda x: "red" if x == "Offense" else "blue")
+        self.id2info = dict(zip(ids, zip(predictions, names, colors)))
+                
+        for id in ids: 
+            if id in self.o_play["nfl_id"].unique(): # players that form part of both the input and the output
+                self.play[id] = pl.concat([self.i_play.filter(pl.col("nfl_id") == id)[["x", "y"]], self.o_play.filter(pl.col("nfl_id") == id)[["x", "y"]]])
+                self.play[id].insert_column(self.play[id].width, pl.Series("color", [self.id2info[id][-1]]*(self.i_frames+self.o_frames)))
+                
+            else: # players that form part of only the input 
+                pad_element = pl.concat([self.i_play.filter(pl.col("nfl_id") == id)[["x", "y"]][-1]]*self.o_frames)
+                self.play[id] = pl.concat([self.i_play.filter(pl.col("nfl_id") == id)[["x", "y"]], pad_element])
+                self.play[id].insert_column(self.play[id].width, pl.Series("color", [self.id2info[id][-1] if i < self.i_frames else "grey" for i in range(self.i_frames+self.o_frames)]))
 
-        self.n_players = len(players)
-        self.out_players = [k for k, v in players.items() if v[0]]
-        self.n_out_players = sum(1 for v in players.values() if v[0])
-        return players
-    
-    def _create_graph(self, players: Dict[str, str] ) -> nx.Graph: 
+    def _create_graph(self) -> nx.Graph: 
         G = nx.Graph()
-        G.add_nodes_from(players.keys())
-        nx.set_node_attributes(G, values={}, name = "pos")
-        nx.set_node_attributes(G, values=players.keys(), name="name")
+        G.add_nodes_from(self.play.keys())
+        nx.set_node_attributes(G, values={}, name="pos")
+        nx.set_node_attributes(G, values=self.id2name, name="name")
         return G 
     
     def _update(self, traces_in, traces_out, frame_id) -> None:
         if frame_id: 
            self.ax.clear() 
+           
+        node_positions = {}
+        for k in self.play.keys(): 
+            node_positions[k] = self.play[k][frame_id].row(0)
         
-        in_frames = self.i_frames
-        if frame_id <= in_frames: # input part of the sequence  
-            play = self.i_play.filter(pl.col("frame_id") == frame_id)
-            # if self.orientation: 
-            #     orientations = play["dir"] 
-            #     orientations = list(map(self._ang2vec, orientations))
-            #     node_orientations = dict(zip(self.players, orientations))
-        else: # output part of the sequence 
-            play = self.o_play.filter(pl.col("frame_id") == frame_id-in_frames)
+        nx.draw_networkx_nodes(G=self.G, pos=node_positions, ax=self.ax, node_color=self.id2color.values())
         
-        x_pos, y_pos = play["x", "y"]
-        pos = list(zip(x_pos, y_pos))
+
+        # in_frames = self.i_frames
+        # if frame_id <= in_frames: # input part of the sequence  
+        #     play = self.i_play.filter(pl.col("frame_id") == frame_id)
+        #     # if self.orientation: 
+        #     #     orientations = play["dir"] 
+        #     #     orientations = list(map(self._ang2vec, orientations))
+        #     #     node_orientations = dict(zip(self.players, orientations))
+        # else: # output part of the sequence 
+        #     play = self.o_play.filter(pl.col("frame_id") == frame_id-in_frames)
         
-        if frame_id <= in_frames: # input
-            node_positions = dict(zip(self.players.keys(), pos)) # all players
-            if frame_id == in_frames: 
-                self.last_node_positions = node_positions # saving the final positions of all players (only appearing in the input sequence)
-                # self.last_x_pos = x_pos
-                # self.last_y_pos = y_pos
-        elif frame_id > in_frames: # output
-            node_positions = dict(zip(self.out_players, pos)) # only out players
-            for player in self.players.keys(): 
-                if player in self.out_players: 
-                    self.last_node_positions[player] = node_positions[player]
-                else: 
-                    self.players[player] = *self.players[player][:2], "grey"
-            node_positions = self.last_node_positions
+        # x_pos, y_pos = play["x", "y"]
+        # pos = list(zip(x_pos, y_pos))
+        
+        # if frame_id <= in_frames: # input
+        #     node_positions = dict(zip(self.players.keys(), pos)) # all players
+        #     if frame_id == in_frames: 
+        #         self.last_node_positions = node_positions # saving the final positions of all players (only appearing in the input sequence)
+        #         # self.last_x_pos = x_pos
+        #         # self.last_y_pos = y_pos
+        # elif frame_id > in_frames: # output
+        #     node_positions = dict(zip(self.out_players, pos)) # only out players
+        #     for player in self.players.keys(): 
+        #         if player in self.out_players: 
+        #             self.last_node_positions[player] = node_positions[player]
+        #         else: 
+        #             self.players[player] = *self.players[player][:2], "grey"
+        #     node_positions = self.last_node_positions
             
 
         
-        # for node, attrs in self.G.nodes(data=True):  # draw direction vectors as arrows
-        #     x, y = attrs.get("pos")
-
-        #     traces_in[node].append((x, y)) # list of past input positions
-        #     x_trace_in = [pos[0] for pos in traces_in[node]]
-        #     y_trace_in = [pos[1] for pos in traces_in[node]]
-        #     ax.plot(x_trace_in, y_trace_in, "r--") # plot trace for player movements before ball is thrown 
+        # # for node, attrs in self.G.nodes(data=True):  # draw direction vectors as arrows
+        # #     x, y = attrs.get("pos")
+        # #     traces_in[node].append((x, y)) # list of past input positions
+        # #     x_trace_in = [pos[0] for pos in traces_in[node]]
+        # #     y_trace_in = [pos[1] for pos in traces_in[node]]
+        # #     ax.plot(x_trace_in, y_trace_in, "r--") # plot trace for player movements before ball is thrown 
             
-        #     if frame > len(n_input_frames): 
-        #         traces_out[node].append((x, y)) # list of past input positions
-        #         x_trace_out = [pos[0] for pos in traces_out[node]]
-        #         y_trace_out = [pos[1] for pos in traces_out[node]]
-        #         ax.plot(x_trace_out, y_trace_out, "b--") # plot trace for player movements when ball is in the air
+        # #     if frame > len(n_input_frames): 
+        # #         traces_out[node].append((x, y)) # list of past input positions
+        # #         x_trace_out = [pos[0] for pos in traces_out[node]]
+        # #         y_trace_out = [pos[1] for pos in traces_out[node]]
+        # #         ax.plot(x_trace_out, y_trace_out, "b--") # plot trace for player movements when ball is in the air
         
-        self.ax.set_title(f"Animation for play {self.play_id} from game {self.game_id} from week {self.week_id}")
-        self.text = self.ax.text(0.9, 1.0, f"Frame:", transform=self.ax.transAxes, fontsize=12, color="black")
-        # self.text = self.ax.text(0.97, 1.0, f"{frame_id}", transform=self.ax.transAxes, fontsize=12, color=color)
+        # self.ax.set_title(f"Animation for play {self.play_id} from game {self.game_id} from week {self.week_id}")
+        # self.text = self.ax.text(0.9, 1.0, f"Frame:", transform=self.ax.transAxes, fontsize=12, color="black")
+        # # self.text = self.ax.text(0.97, 1.0, f"{frame_id}", transform=self.ax.transAxes, fontsize=12, color=color)
 
         
         self.ax.imshow(self.bg_img, extent=[-1, 1, -1, 1], aspect="auto")
-        # ax.plot(x_pos, y_pos, "r--")
-        # nx.set_node_attributes(self.G, node_positions, name="pos")
-        # x, y = list(node_positions.values())[0]
-        #self.x.append(x)
-        # self.y.append(y)
+        # # ax.plot(x_pos, y_pos, "r--")
+        # # nx.set_node_attributes(self.G, node_positions, name="pos")
+        # # x, y = list(node_positions.values())[0]
+        # #self.x.append(x)
+        # # self.y.append(y)
         
-        nx.draw_networkx_edges(G=self.G, pos=node_positions, ax=self.ax, edge_color="black")
-        nx.draw_networkx_nodes(G=self.G, pos=node_positions, node_color=[v[-1] for v in self.players.values()], ax=self.ax)
+        # nx.draw_networkx_edges(G=self.G, pos=node_positions, ax=self.ax, edge_color="black")
+        # nx.draw_networkx_nodes(G=self.G, pos=node_positions, node_color=[v[-1] for v in self.players.values()], ax=self.ax)
 
-        # nx.draw_networkx_nodes(G=self.G, pos=node_positions, node_color=[c for _, _, c in self.players.values()], ax=self.ax)
+        # # nx.draw_networkx_nodes(G=self.G, pos=node_positions, node_color=[c for _, _, c in self.players.values()], ax=self.ax)
 
-        # shapes = list(set(value[0] for value in self.players.values()))
+        # # shapes = list(set(value[0] for value in self.players.values()))
         
-        # for k, v in self.players.items(): 
-        #     self.groups[v[0]][k] = v
+        # # for k, v in self.players.items(): 
+        # #     self.groups[v[0]][k] = v
         
-        # for k_1, v_1 in self.groups.items(): 
-        #     for k_2, v_2 in v_1.items(): 
-        #         pos = node_positions
+        # # for k_1, v_1 in self.groups.items(): 
+        # #     for k_2, v_2 in v_1.items(): 
+        # #         pos = node_positions
 
-        #     nx.draw_networkx_nodes(G=self.G, pos=node_positions, node_color=[c for _, _, c in self.players.values()], ax=self.ax)
-        # nx.draw_networkx_labels(G=self.G, pos=node_positions, labels=self.players.keys(), ax=self.ax, font_size=10, font_color="black")
-        # ax.plot(self.x, self.y, "r--", lw=5)
+        # #     nx.draw_networkx_nodes(G=self.G, pos=node_positions, node_color=[c for _, _, c in self.players.values()], ax=self.ax)
+        # # nx.draw_networkx_labels(G=self.G, pos=node_positions, labels=self.players.keys(), ax=self.ax, font_size=10, font_color="black")
+        # # ax.plot(self.x, self.y, "r--", lw=5)
 
     def _normalize(self, df: pl.DataFrame, extrema: DictConfig) -> pl.DataFrame:
         return df.with_columns([
@@ -169,7 +181,7 @@ class Visualize():
         n_frames = self.i_frames+self.o_frames
          
         plt.axis("equal")
-        self.anim = animation.FuncAnimation(self.fig, partial(self._update, i_traces, o_traces), frames=range(1, n_frames+1), repeat=False, blit=False)
+        self.anim = animation.FuncAnimation(self.fig, partial(self._update, i_traces, o_traces), frames=range(0, n_frames), repeat=False, blit=False)
         self.animation.fps *= n_frames
         self.anim.save(**self.animation)
    
