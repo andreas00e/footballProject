Rotation of ball should be fixed now :))!

visualization/visualize.py
@@ -4,12 +4,14 @@ import hydra
 import random
 import logging 
 import numpy as np 
+from numpy.linalg import norm 
 import polars as pl 
 from omegaconf import DictConfig, OmegaConf
 from typing import Dict, List, Tuple, Union
 
+import cv2
 import networkx as nx
-import matplotlib
+# import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import matplotlib.animation as animation
@@ -81,7 +83,7 @@ class Visualize():
             qb_id = ids[positions.index_of("QB")]
             qb_rows = self.i_play.filter(pl.col("nfl_id") == qb_id)
             ground_pos = qb_rows[["x", "y"]]
-            ground_angle = list(qb_rows["o"]*360.0)
+            ground_angle = list(360-qb_rows["o"])
             p1 = np.asarray(ground_pos[-1].row(0))
             p2 = np.asarray(ball_land.row(0))
             p = p2-p1
@@ -90,8 +92,13 @@ class Visualize():
             self.ball = pl.concat([ground_pos, air_pos])
             self.ball.insert_column(self.ball.width, pl.Series("color", ["brown" for _ in range(self.i_frames+self.o_frames)]))
             
-            air_angle = np.arccos(p[1]/np.linalg.norm(p))/(2*np.pi)*360
-            air_angle = [float(air_angle)]*self.o_frames
+            air_angle = np.arctan2(p[1], p[0])
+            if p[0] < 0: 
+                air_angle += 3/2*np.pi 
+            elif p[0] >= 0: 
+                air_angle += np.sign(p[1]/p[0])*3/2*np.pi
+                
+            air_angle = [np.degrees(air_angle)]*self.o_frames
             self.ball_angle = ground_angle+air_angle
 
     def _create_graph(self) -> nx.Graph: 
@@ -102,6 +109,7 @@ class Visualize():
         return G 
     
     def _update(self, frame_id) -> None:
+        # print(self.ball_angle[frame_id])
         if frame_id: 
            self.ax.clear() 
            
@@ -115,7 +123,8 @@ class Visualize():
     
         nx.draw_networkx_nodes(G=self.G, pos=node_positions, node_color=node_color, ax=self.ax)
         
-        r_ball_img = np.clip(rotate(self.ball_img, angle=self.ball_angle[frame_id], axes=(0, 1)), a_min=0, a_max=1)
+        ball_img = cv2.resize(self.ball_img, (125, 167))
+        r_ball_img = np.clip(rotate(ball_img, angle=self.ball_angle[frame_id], axes=(0, 1)), a_min=0, a_max=1)
         ib = OffsetImage(r_ball_img, zoom=0.1)
         ab = AnnotationBbox(ib, self.ball[frame_id][["x", "y"]].row(0), frameon=False)
         self.ax.add_artist(ab)